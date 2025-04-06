92b6759b563b176ed7487bf7089736d9dfff75f2
CigdemOzel <cigdemozel17@gmail.com>
2025-04-06 16:15:59 +0300

    update

---
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -0,0 +1,45 @@
+package com.workintech.fswebs17d1.controller;
+
+import com.workintech.fswebs17d1.entity.Animal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/workintech/animal")
+public class AnimalController {
+
+    private Map<Integer, Animal> animals = new HashMap<Integer, Animal>();
+
+    @GetMapping
+    public List<Animal> getAllAnimals() {
+        return new ArrayList<>(animals.values());
+    }
+
+    @GetMapping("{id}")
+    public Animal getAnimalById(@PathVariable int id) {
+        return animals.get(id);
+    }
+
+    @PostMapping
+    public Animal addAnimal(@RequestParam int id, @RequestParam String name) {
+        Animal animal = new Animal(id,name);
+        animals.put(id,animal);
+        return animal;
+    }
+
+    @PutMapping("/{id}")
+    public Animal updateAnimal(@PathVariable int id, @RequestBody Animal updateAnimal) {
+        animals.put(id, updateAnimal);
+        return updateAnimal;
+    }
+
+    @DeleteMapping("/{id}")
+    public Animal deleteAnimal(@PathVariable int id) {
+        return animals.remove(id);
+    }
+
+}
